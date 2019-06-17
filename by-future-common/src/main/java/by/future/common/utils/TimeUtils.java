a9a0e3c2cd0495361872acc83ea61f67a1bca1dc
bydeng <bydeng@ctrip.com>
2019-06-17 13:19:14 +0800

    udpate

---
+++ by-future-common/src/main/java/by/future/common/utils/TimeUtils.java
@@ -0,0 +1,256 @@
+package by.future.common.utils;
+
+
+import java.util.*;
+
+/**
+ * 时间工具类
+ *
+ * @Author：by@Deng
+ * @Date：2019/6/15 11:52
+ */
+public class TimeUtils {
+
+
+    /**
+     * 获取某个日期的开始时间
+     *
+     */
+    public static Date getDayStartTime(Date d) {
+        Calendar calendar = Calendar.getInstance();
+        if(null != d) calendar.setTime(d);
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
+        return calendar.getTime();
+    }
+
+
+    /**
+     * 获取某个日期的结束时间
+     *
+     */
+    public static Date getDayEndTime(Date d) {
+        Calendar calendar = Calendar.getInstance();
+        if(null != d) calendar.setTime(d);
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),    calendar.get(Calendar.DAY_OF_MONTH), 23, 59, 59);
+        return calendar.getTime();
+    }
+
+
+    /**
+     * 获取某个日期是周几，1：周一，7：周日
+     *
+     */
+    public static int getDayOfWeek(Date date){
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE,-1);
+        return cal.get(Calendar.DAY_OF_WEEK);
+    }
+
+
+    /**
+     * 获取本周从周一的开始时间
+     *
+     */
+    @SuppressWarnings("unused")
+    public static Date getBeginDayOfWeek() {
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int dayofweek = cal.get(Calendar.DAY_OF_WEEK);
+        if (dayofweek == 1) {
+            dayofweek += 7;
+        }
+        cal.add(Calendar.DATE, 2 - dayofweek);
+        return getDayStartTime(cal.getTime());
+    }
+
+
+    /**
+     * 获取本周从周一的结束时间
+     *
+     */
+    public static Date getEndDayOfWeek(){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(getBeginDayOfWeek());
+        cal.add(Calendar.DAY_OF_WEEK, 6);
+        Date weekEndSta = cal.getTime();
+        return getDayEndTime(weekEndSta);
+    }
+
+
+    /**
+     * 获取本月的开始时间
+     *
+     */
+    public static Date getBeginDayOfMonth() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(getNowYear(), getNowMonth() - 1, 1);
+        return getDayStartTime(calendar.getTime());
+    }
+
+
+    /**
+     * 获取本月的结束时间
+     *
+     */
+    public static Date getEndDayOfMonth() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(getNowYear(), getNowMonth() - 1, 1);
+        int day = calendar.getActualMaximum(5);
+        calendar.set(getNowYear(), getNowMonth() - 1, day);
+        return getDayEndTime(calendar.getTime());
+    }
+
+
+    /**
+     * 获取本年的开始时间
+     *
+     */
+    public static Date getBeginDayOfYear() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, getNowYear());
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.DATE, 1);
+
+        return getDayStartTime(cal.getTime());
+    }
+
+
+    /**
+     * 获取本年的结束时间
+     *
+     */
+    public static Date getEndDayOfYear() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, getNowYear());
+        cal.set(Calendar.MONTH, Calendar.DECEMBER);
+        cal.set(Calendar.DATE, 31);
+        return getDayEndTime(cal.getTime());
+    }
+
+
+    /**
+     * 获取今年是哪一年
+     *
+     */
+    public static int getNowYear() {
+        Date date = new Date();
+        GregorianCalendar gc = (GregorianCalendar) Calendar.getInstance();
+        gc.setTime(date);
+        return Integer.valueOf(gc.get(1));
+    }
+
+
+    /**
+     * 获取本月是哪一月
+     *
+     */
+    public static int getNowMonth() {
+        Date date = new Date();
+        GregorianCalendar gc = (GregorianCalendar) Calendar.getInstance();
+        gc.setTime(date);
+        return gc.get(2) + 1;
+    }
+
+
+    /**
+     * 两个日期相减得到的天数
+     *
+     */
+    public static int getDiffDays(Date beginDate, Date endDate) {
+
+        if (beginDate == null || endDate == null) {
+            throw new IllegalArgumentException("getDiffDays param is null!");
+        }
+
+        long diff = (endDate.getTime() - beginDate.getTime())
+                / (1000 * 60 * 60 * 24);
+
+        int days = new Long(diff).intValue();
+
+        return days;
+    }
+
+
+    /**
+     * 两个日期相减得到的毫秒数
+     *
+     */
+    public static long dateDiff(Date beginDate, Date endDate) {
+        long date1ms = beginDate.getTime();
+        long date2ms = endDate.getTime();
+        return date2ms - date1ms;
+    }
+
+
+    /**
+     * 返回某月该季度的第一个月
+     *
+     */
+    public static Date getFirstSeasonDate(Date date) {
+        final int[] SEASON = { 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4 };
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int sean = SEASON[cal.get(Calendar.MONTH)];
+        cal.set(Calendar.MONTH, sean * 3 - 3);
+        return cal.getTime();
+    }
+
+
+    /**
+     * 获取某年某月到某年某月按天的切片日期集合（间隔天数的集合）
+     *
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public static List getTimeList(int beginYear, int beginMonth, int endYear,
+                                   int endMonth, int k) {
+        List list = new ArrayList();
+        if (beginYear == endYear) {
+            for (int j = beginMonth; j <= endMonth; j++) {
+                list.add(getTimeList(beginYear, j, k));
+
+            }
+        } else {
+            {
+                for (int j = beginMonth; j < 12; j++) {
+                    list.add(getTimeList(beginYear, j, k));
+                }
+
+                for (int i = beginYear + 1; i < endYear; i++) {
+                    for (int j = 0; j < 12; j++) {
+                        list.add(getTimeList(i, j, k));
+                    }
+                }
+                for (int j = 0; j <= endMonth; j++) {
+                    list.add(getTimeList(endYear, j, k));
+                }
+            }
+        }
+        return list;
+    }
+
+
+    /**
+     * 获取某年某月按天切片日期集合（某个月间隔多少天的日期集合）
+     *
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static List getTimeList(int beginYear, int beginMonth, int k) {
+        List list = new ArrayList();
+        Calendar begincal = new GregorianCalendar(beginYear, beginMonth, 1);
+        int max = begincal.getActualMaximum(Calendar.DATE);
+        for (int i = 1; i < max; i = i + k) {
+            list.add(begincal.getTime());
+            begincal.add(Calendar.DATE, k);
+        }
+        begincal = new GregorianCalendar(beginYear, beginMonth, max);
+        list.add(begincal.getTime());
+        return list;
+    }
+
+
+
+
+}
